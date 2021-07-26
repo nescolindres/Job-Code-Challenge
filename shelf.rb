6c2fd4fb3a69aae84fa8d18fd7d35fc9a45c1dd4
nescolindres <44357907+nescolindres@users.noreply.github.com>
2021-07-25 20:21:12 -0700

    Add files via upload
    Should be final version before pushing to master

--- shelf.rb
+++ shelf.rb
@@ -19,12 +19,20 @@ $candyArray
     @candyArray[shelf] = [value] #If i have the same index, im actually just appending the value.
   end                               #{"1" => ["candy", "coco"]  }
 
+  def deleteElement(shelf , value)
+    @candyArray[shelf].delete(value)
+  end
+
   def getArray
   return @candyArray
   end
 
 def appendElement (shelf, candy)
  @candyArray[shelf] << candy
+end
+
+def removeArray (shelfNumber)
+@candyArray.delete(shelfNumber)
 end
 
 end
